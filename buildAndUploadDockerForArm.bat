3942608775a24af13b1261a761b5bad58bd0a22e
SeppPenner <HansM2013@gmx.de>
2024-02-20 18:48:29 +0100

    Updated version in Docker build, added binaries. Fixes https://github.com/SeppPenner/CalDavSynologySyncer/issues/2.

--- buildAndUploadDockerForArm.bat
+++ buildAndUploadDockerForArm.bat
@@ -1,7 +1,7 @@
 cd src\CalDavSynologySyncer
 dotnet publish -c Release --output publish/ -r linux-arm --no-self-contained
-docker build --tag sepppenner/caldavsynologysyncer-arm:1.0.0 -f Dockerfile.armv7 .
+docker build --tag sepppenner/caldavsynologysyncer-arm:1.1.0 -f Dockerfile.armv7 .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/caldavsynologysyncer-arm:1.0.0
+docker push sepppenner/caldavsynologysyncer-arm:1.1.0
 @ECHO.Build successful. Press any key to exit.
 pause
